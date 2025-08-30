77de6f6284ae86a7084d1d372d6723c9ea9a46b4
devops <devops123@gmail.com>
2025-08-30 12:59:35 +0500

    update the fiels app.py and prometheus.yml

--- src/app.py
+++ src/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
-@app.route("/")
+# Example metric
+REQUEST_COUNT = Counter('request_count', 'Total number of requests')
+
+@app.route('/')
 def home():
-    return "Hello from DevOps Intern Project!"
+    REQUEST_COUNT.inc()
+    return "Hello from Flask CI/CD App!"
+
+# Metrics endpoint for Prometheus
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
